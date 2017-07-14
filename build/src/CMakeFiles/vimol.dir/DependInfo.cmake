d8182ce7ad0b48ba074b92ee5c00fb12640cb6bf
filipecosta90 <filipecosta.90@gmail.com>
2017-07-14 14:14:53 +0100

    added cmake compatibility

---
+++ build/src/CMakeFiles/vimol.dir/DependInfo.cmake
@@ -0,0 +1,50 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/filipeoliveira/Documents/vimol/src/atoms.c" "/Users/filipeoliveira/Documents/vimol/build/src/CMakeFiles/vimol.dir/atoms.c.o"
+  "/Users/filipeoliveira/Documents/vimol/src/bind.c" "/Users/filipeoliveira/Documents/vimol/build/src/CMakeFiles/vimol.dir/bind.c.o"
+  "/Users/filipeoliveira/Documents/vimol/src/camera.c" "/Users/filipeoliveira/Documents/vimol/build/src/CMakeFiles/vimol.dir/camera.c.o"
+  "/Users/filipeoliveira/Documents/vimol/src/cmd.c" "/Users/filipeoliveira/Documents/vimol/build/src/CMakeFiles/vimol.dir/cmd.c.o"
+  "/Users/filipeoliveira/Documents/vimol/src/edit.c" "/Users/filipeoliveira/Documents/vimol/build/src/CMakeFiles/vimol.dir/edit.c.o"
+  "/Users/filipeoliveira/Documents/vimol/src/error.c" "/Users/filipeoliveira/Documents/vimol/build/src/CMakeFiles/vimol.dir/error.c.o"
+  "/Users/filipeoliveira/Documents/vimol/src/exec.c" "/Users/filipeoliveira/Documents/vimol/build/src/CMakeFiles/vimol.dir/exec.c.o"
+  "/Users/filipeoliveira/Documents/vimol/src/formats.c" "/Users/filipeoliveira/Documents/vimol/build/src/CMakeFiles/vimol.dir/formats.c.o"
+  "/Users/filipeoliveira/Documents/vimol/src/graph.c" "/Users/filipeoliveira/Documents/vimol/build/src/CMakeFiles/vimol.dir/graph.c.o"
+  "/Users/filipeoliveira/Documents/vimol/src/history.c" "/Users/filipeoliveira/Documents/vimol/build/src/CMakeFiles/vimol.dir/history.c.o"
+  "/Users/filipeoliveira/Documents/vimol/src/main.c" "/Users/filipeoliveira/Documents/vimol/build/src/CMakeFiles/vimol.dir/main.c.o"
+  "/Users/filipeoliveira/Documents/vimol/src/pair.c" "/Users/filipeoliveira/Documents/vimol/build/src/CMakeFiles/vimol.dir/pair.c.o"
+  "/Users/filipeoliveira/Documents/vimol/src/rec.c" "/Users/filipeoliveira/Documents/vimol/build/src/CMakeFiles/vimol.dir/rec.c.o"
+  "/Users/filipeoliveira/Documents/vimol/src/sel.c" "/Users/filipeoliveira/Documents/vimol/build/src/CMakeFiles/vimol.dir/sel.c.o"
+  "/Users/filipeoliveira/Documents/vimol/src/settings.c" "/Users/filipeoliveira/Documents/vimol/build/src/CMakeFiles/vimol.dir/settings.c.o"
+  "/Users/filipeoliveira/Documents/vimol/src/spi.c" "/Users/filipeoliveira/Documents/vimol/build/src/CMakeFiles/vimol.dir/spi.c.o"
+  "/Users/filipeoliveira/Documents/vimol/src/state.c" "/Users/filipeoliveira/Documents/vimol/build/src/CMakeFiles/vimol.dir/state.c.o"
+  "/Users/filipeoliveira/Documents/vimol/src/statusbar.c" "/Users/filipeoliveira/Documents/vimol/build/src/CMakeFiles/vimol.dir/statusbar.c.o"
+  "/Users/filipeoliveira/Documents/vimol/src/sys.c" "/Users/filipeoliveira/Documents/vimol/build/src/CMakeFiles/vimol.dir/sys.c.o"
+  "/Users/filipeoliveira/Documents/vimol/src/tabs.c" "/Users/filipeoliveira/Documents/vimol/build/src/CMakeFiles/vimol.dir/tabs.c.o"
+  "/Users/filipeoliveira/Documents/vimol/src/tok.c" "/Users/filipeoliveira/Documents/vimol/build/src/CMakeFiles/vimol.dir/tok.c.o"
+  "/Users/filipeoliveira/Documents/vimol/src/undo.c" "/Users/filipeoliveira/Documents/vimol/build/src/CMakeFiles/vimol.dir/undo.c.o"
+  "/Users/filipeoliveira/Documents/vimol/src/util.c" "/Users/filipeoliveira/Documents/vimol/build/src/CMakeFiles/vimol.dir/util.c.o"
+  "/Users/filipeoliveira/Documents/vimol/src/vec.c" "/Users/filipeoliveira/Documents/vimol/build/src/CMakeFiles/vimol.dir/vec.c.o"
+  "/Users/filipeoliveira/Documents/vimol/src/view.c" "/Users/filipeoliveira/Documents/vimol/build/src/CMakeFiles/vimol.dir/view.c.o"
+  "/Users/filipeoliveira/Documents/vimol/src/xmalloc.c" "/Users/filipeoliveira/Documents/vimol/build/src/CMakeFiles/vimol.dir/xmalloc.c.o"
+  "/Users/filipeoliveira/Documents/vimol/src/yank.c" "/Users/filipeoliveira/Documents/vimol/build/src/CMakeFiles/vimol.dir/yank.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "src"
+  "../src"
+  "/usr/local/Cellar/cairo/1.14.10/include/cairo"
+  "/usr/local/Cellar/sdl2/2.0.5/include/SDL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
